990f67c42e33b86f189313f25c8ed3cbc60fa520
Evan MacDonald <evan.t.macdonald01@gmail.com>
2022-10-11 09:02:11 -0300

    Fixed Repository

---
+++ Conditionals/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conditionals: ");
+            Console.WriteLine("--------------");
+            Console.WriteLine();
+
+            int score;
+            string gamertag;
+
+            score = 0;
+            gamertag = "Non-Gamer";
+            gamertag = "EvanSeven2003";
+            if (gamertag == "EvanSeven2003")
+            {
+                score = 1000000; // Cheating >;)
+            }
+            Console.WriteLine("Score: " + score);
+
+            Console.ReadKey(true);
+        }
+    }
+}
